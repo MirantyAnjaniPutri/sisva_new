1ddbd0916819a039862481661549a94d44f049cb
MirantyAnjaniPutri <miranty.anjani@ui.ac.id>
2023-11-21 08:06:13 +0700

    Section 1 Hero almost done. Navbar and footer done.

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Section1Hero from "../components/desktop/landing_page/section1_hero";
+
+export default function Home() {
+  return (
+    <div className="">
+      <Section1Hero />
+      <h1>Hello World</h1>
+    </div>
+  );
+}
